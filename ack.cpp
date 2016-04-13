4b169f8033f766358ca3a2be41238ff7a772530c
Bernardo Meurer <meurerbernardo@gmail.com>
2016-04-13 18:42:58 -0300

    Fixed bugs. Still overflows

--- ack.cpp
+++ ack.cpp
@@ -18,18 +18,18 @@ int main(int argc, char *argv[]) {
             std::cout << "A(" << m << ", " << n << ")" << std::endl;
         }
     }*/
-    std::cout << ackermann(4, 1) << std::endl; //Currently breaks life
+    std::cout << ackermann(4, 2) << std::endl; //Currently breaks life
 }
 
 mpz_int ackermann(mpz_int m, mpz_int n) {
     if (m == 0) {
-        std::cout << "Route 1: m = " << m << ", n= " << n << std::endl;
+        //std::cout << "Route 1: m = " << m << ", n= " << n << std::endl;
         return n + 1;
     }
     if (n == 0) {
-        std::cout << "Route 2: m = " << m << ", n= " << n << std::endl;
+        //std::cout << "Route 2: m = " << m << ", n= " << n << std::endl;
         return (ackermann(m - 1, 1));
     }
-    std::cout << "Route 3: m = " << m << ", n= " << n << std::endl;
+    //std::cout << "Route 3: m = " << m << ", n= " << n << std::endl;
     return (ackermann(m - 1, ackermann(m, n - 1)));
 }
