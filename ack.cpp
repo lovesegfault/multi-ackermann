8e32065f50c5740667d544b6f415a4f935a94599
Bernardo Meurer <meurerbernardo@gmail.com>
2016-04-13 18:29:49 -0300

    Added debug code

--- ack.cpp
+++ ack.cpp
@@ -11,23 +11,27 @@ mpz_int ackermann(mpz_int m, mpz_int n);
 
 int main(int argc, char *argv[]) {
     // int coreCount = boost::thread::hardware_concurrency();
-
+    /*
     for (mpz_int m = 4; m >= 0; ++m) {
         for (mpz_int n = 0; n <= 10; ++n) {
             ackermann(m, n);
             std::cout << "A(" << m << ", " << n << ")" << std::endl;
         }
-    }
+    }*/
+    ackermann(4, 1);
 }
 
 mpz_int ackermann(mpz_int m, mpz_int n) {
     if (m == 0) {
+        std::cout << "Route 1: m = " << m << ", n= " << n << std::endl;
         return n + 1;
     }
     if (m > 0 && n == 0) {
+        std::cout << "Route 2: m = " << m << ", n= " << n << std::endl;
         return (ackermann(m - 1, 1));
     }
     else {
+        std::cout << "Route 3: m = " << m << ", n= " << n << std::endl;
         return (ackermann(m - 1, ackermann(m, n - 1)));
     }
 }
