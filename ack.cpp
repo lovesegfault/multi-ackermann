f06eb99008adc46b3f5e7fbc8b9941aeb385ed22
Bernardo Meurer <meurerbernardo@gmail.com>
2016-06-26 17:06:13 -0300

    Worked on non-recursive ackermann

--- ack.cpp
+++ ack.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <gmpxx.h>
-#include <stdlib.h>
-#include <libltdl/lt_system.h>
+
 
 mpz_class ackermann(mpz_class m, mpz_class n);
 
+mpz_class ack(mpz_class i, mpz_class n);
+
+long acker(long i, long n);
 
 int main(int argc, char *argv[]) {
-
+    //std::cout << ackermann(3, 1).get_str(10);
+    std::cout << acker(3, 5);
+    /*
     for (mpz_class m = 1; mpz_cmp_ui(m.get_mpz_t(), 0) >= 0; ++m) {
         for (mpz_class n = 0; mpz_cmp_ui(n.get_mpz_t(), 10) <= 0; ++n) {//n<= 10
             std::cout << "A(" << m.get_str(10) << ", " << n.get_str(10) << ") = " << ackermann(m, n).get_str(10) <<
             std::endl;
         }
-    }
+    }*/
 }
 
-//TODO: Get the god damn clears working
 mpz_class ackermann(mpz_class m, mpz_class n) {
     if (mpz_cmp_ui(m.get_mpz_t(), 0) == 0) {
         //std::cout << "Route 1: m = " << m.get_str(10) << ", n= " << n.get_str(10) << std::endl;
@@ -40,47 +43,52 @@ mpz_class ackermann(mpz_class m, mpz_class n) {
     }
 }
 
-int ack_nonrecursive(int m, int n, int *status) {
-    int *value = NULL;
-    size_t size = 0;
 
-    for (; ;) {
-        if (m == 0) {
-            n++;
-            if (size-- == 0) {
-                *status = EXIT_SUCCESS;
-                break;
+long acker(long i, long n) {
+    long next[i + 1] = {0};
+    long goal[i + 1] = {0}; //What the fuck happens here??
+    long value = 0;
+    std::fill_n(goal, i - 1, 1);
+    goal[i] = -1;
+    //std::cout<<(goal[i]);
+    while (next[i] != n + 1) {
+        value = next[0] + 1;
+        bool tranferring = true;
+        long current = 0;
+        while (tranferring) {
+            if (next[current] == goal[current]) {
+                goal[current] = value;
             }
-            m = value[size];
-            continue;
-        }
-
-        if (n == 0) {
-            m--;
-            n = 1;
-            continue;
-        }
-
-        size_t index = size++;
-        if ((size & index) == 0) {
-            if (size >= SIZE_MAX / sizeof *value) {
-                *status = EXIT_FAILURE;
-                break;
+            else {
+                tranferring = false;
             }
-
-            void *temp = realloc(value, (2 * size + 1) * sizeof *value);
-            if (temp == NULL) {
-                *status = EXIT_FAILURE;
-                break;
-            }
-
-            value = temp;
+            next[current] += 1;
+            current += 1;
         }
-
-        value[index] = m - 1;
-        n--;
     }
+    return value;
+}
 
-    free(value);
-    return n;
+
+mpz_class ack(mpz_class i, mpz_class n) {
+    mpz_class next[i.get_ui() + 1] = {0};
+    mpz_class goal[i.get_ui()] = {1};
+    mpz_class value;
+    goal[i.get_ui()] = -1;
+    while (mpz_cmp(next[i.get_ui()].get_mpz_t(), n.get_mpz_t()) != 0) {
+        mpz_add_ui(value.get_mpz_t(), next[0].get_mpz_t(), 1);
+        bool transferring = true;
+        mpz_class current = 0;
+        while (transferring) {
+            if (mpz_cmp(next[current.get_ui()].get_mpz_t(), goal[current.get_ui()].get_mpz_t()) == 0) {
+                goal[current.get_ui()] = value;
+            }
+            else {
+                transferring = false;
+            }
+            next[current.get_ui()] += 1;
+            current += 1;
+        }
+    }
+    return value;
 }
