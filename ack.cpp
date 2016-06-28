7841951d4614a74b22ba8e753e1bc6fc60404a6e
Bernardo Meurer <meurerbernardo@gmail.com>
2016-06-27 23:50:44 -0300

    Finished iterative, arbitrary precision implementation

--- ack.cpp
+++ ack.cpp
@@ -1,81 +1,34 @@
 #include <iostream>
 #include <gmpxx.h>
 
+mpz_class ackermann(mpz_class i, mpz_class n);
 
-mpz_class ackermann(mpz_class m, mpz_class n);
-
-mpz_class ack(mpz_class i, mpz_class n);
-
-long acker(long i, long n);
 
 int main(int argc, char *argv[]) {
-    //std::cout << ackermann(3, 1).get_str(10);
-    std::cout << acker(3, 5);
-    /*
+
     for (mpz_class m = 1; mpz_cmp_ui(m.get_mpz_t(), 0) >= 0; ++m) {
         for (mpz_class n = 0; mpz_cmp_ui(n.get_mpz_t(), 10) <= 0; ++n) {//n<= 10
             std::cout << "A(" << m.get_str(10) << ", " << n.get_str(10) << ") = " << ackermann(m, n).get_str(10) <<
             std::endl;
         }
-    }*/
-}
-
-mpz_class ackermann(mpz_class m, mpz_class n) {
-    if (mpz_cmp_ui(m.get_mpz_t(), 0) == 0) {
-        //std::cout << "Route 1: m = " << m.get_str(10) << ", n= " << n.get_str(10) << std::endl;
-        mpz_class z;
-        mpz_add_ui(z.get_mpz_t(), n.get_mpz_t(), 1);
-        return z;
-    }
-    else if (mpz_cmp_ui(n.get_mpz_t(), 0) == 0) {
-        //std::cout << "Route 2: m = " << m.get_str(10) << ", n= " << n.get_str(10) << std::endl;
-        mpz_class z;
-        mpz_sub_ui(z.get_mpz_t(), m.get_mpz_t(), 1);
-        return (ackermann(z, 1));
-    }
-    else {
-        //std::cout << "Route 3: m = " << m.get_str(10) << ", n= " << n.get_str(10) << std::endl;
-        mpz_class z;
-        mpz_sub_ui(z.get_mpz_t(), n.get_mpz_t(), 1);
-        mpz_class y;
-        mpz_sub_ui(y.get_mpz_t(), m.get_mpz_t(), 1);
-        return (ackermann(y, ackermann(m, z)));
     }
 }
 
 
-long acker(long i, long n) {
-    long next[i + 1] = {0};
-    long goal[i + 1] = {0}; //What the fuck happens here??
-    long value = 0;
-    std::fill_n(goal, i - 1, 1);
-    goal[i] = -1;
-    //std::cout<<(goal[i]);
-    while (next[i] != n + 1) {
-        value = next[0] + 1;
-        bool tranferring = true;
-        long current = 0;
-        while (tranferring) {
-            if (next[current] == goal[current]) {
-                goal[current] = value;
-            }
-            else {
-                tranferring = false;
-            }
-            next[current] += 1;
-            current += 1;
-        }
+mpz_class ackermann(mpz_class i, mpz_class n) {
+    mpz_class next[i.get_ui() + 1];
+    mpz_class goal[i.get_ui() + 1];
+    mpz_class value = 0;
+    for (mpz_class count = 0; mpz_cmp_ui(count.get_mpz_t(), i.get_ui()) != 0; count++) {
+        next[count.get_ui()] = 0;
     }
-    return value;
-}
-
-
-mpz_class ack(mpz_class i, mpz_class n) {
-    mpz_class next[i.get_ui() + 1] = {0};
-    mpz_class goal[i.get_ui()] = {1};
-    mpz_class value;
+    for (mpz_class count = 0; mpz_cmp_ui(count.get_mpz_t(), i.get_ui()) != 0; count++) {
+        goal[count.get_ui()] = 1;
+    }
     goal[i.get_ui()] = -1;
-    while (mpz_cmp(next[i.get_ui()].get_mpz_t(), n.get_mpz_t()) != 0) {
+    mpz_class pholder; //For use as intermediate
+    mpz_add_ui(pholder.get_mpz_t(), n.get_mpz_t(), 1);
+    while (mpz_cmp(next[i.get_ui()].get_mpz_t(), pholder.get_mpz_t()) != 0) {
         mpz_add_ui(value.get_mpz_t(), next[0].get_mpz_t(), 1);
         bool transferring = true;
         mpz_class current = 0;
